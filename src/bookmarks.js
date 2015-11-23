d04d21bc480aaf933ad48e8470a39f83fba929dd
Max-Hu <huzhiwise@gmail.com>
2015-11-23 23:00:33 +0800

    js:refactor the code

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -2,48 +2,58 @@ $(function(){
     var lowerCase = new Array();
     var $jsontip = $("#jsonTip");
     $.getJSON("data/bookmarks.json",function(data){
-        var strHtml = "";//存储数据的变量
-        $jsontip.empty();//清空内容
+        $jsontip.empty();
+        var strHtml = "";
         $.each(data,function(infoIndex,info){
-            var myDate = new Date( info["created"] *1000);
-            var realtime = myDate.toLocaleString();
-            strHtml +="<div>";
-            strHtml += "<div class=\'title\'>"+info["title"]+"</div>";
-            strHtml += "<div class=\'time\'>"+"Created @ "+realtime+"</div>";
-            strHtml +="</div>";
-            strHtml += "<hr class=\'hr\'>"
-            info = _.mapObject(info, function(val, key) {
-                return val.toLowerCase();
-            });
-            lowerCase.push(info);
+            var realtime = getRealTime(info["created"] );
+            strHtml += createHtml(info["title"],realtime)
+            pushLowCase(info,lowerCase);
           })
-        $jsontip.html(strHtml);//显示处理后的数据
+          $jsontip.html(strHtml);
       });
 
-    $("#textbox").keyup(function(){
+    $("#textbox").on('input', function (){
         var value = $(this).val();
         $jsontip.empty();
-        // var test = _.filter(lowerCase, function(object,infoIndex){
-        //   return object.title.indexOf(value); });
         $.getJSON("data/bookmarks.json",function(data){
             var strHtml = "";
             $.each(lowerCase,function(infoIndex,info){
                 if (info["title"].indexOf(value.toLowerCase()) !=-1) {
-                    var myDate = new Date( info["created"] *1000);
-                    var realtime = myDate.toLocaleString();
-                    var position = info["title"].indexOf(value.toLowerCase());
-                    var defaultTitle = data[infoIndex].title;
-                    var title =  defaultTitle.substring(0,position) + "<mark>";
-                    title +=defaultTitle.substring(position,position+value.length) + "</mark>";
-                    title += defaultTitle.substring(position+value.length,data[infoIndex].length);
-                    strHtml +="<div>";
-                    strHtml += "<div class=\'title\'>"+title+"</div>";
-                    strHtml += "<div class=\'time\'>"+"Created @ "+realtime+"</div>";
-                    strHtml +="</div>";
-                    strHtml += "<hr class=\'hr\'>"
+                    var realtime = getRealTime(info["created"]);
+                    var title = createHightLight();
+                    strHtml += createHtml(title,realtime);
+                    function createHightLight() {
+                      var position = info["title"].indexOf(value.toLowerCase());
+                      var defaultTitle = data[infoIndex].title;
+                      var title =  defaultTitle.substring(0,position) + "<mark>";
+                      title +=defaultTitle.substring(position,position+value.length) + "</mark>";
+                      return title += defaultTitle.substring(position+value.length,data[infoIndex].length);
+                    }
                 }
               })
               $jsontip.html(strHtml);
       });
       });
+
+      function getRealTime(time) {
+        var myDate = new Date( time *1000);
+        return myDate.toLocaleString();
+      }
+
+      function createHtml(title,realtime) {
+        var strHtml = "";
+        strHtml +="<div>";
+        strHtml += "<div class=\'title\'>"+title+"</div>";
+        strHtml += "<div class=\'time\'>"+"Created @ "+realtime+"</div>";
+        strHtml +="</div>";
+        strHtml += "<hr class=\'hr\'>"
+        return strHtml;
+      }
+
+      function pushLowCase(info,lowerCase) {
+        info = _.mapObject(info, function(val, key) {
+            return val.toLowerCase();
+        });
+        lowerCase.push(info);
+      }
 })
